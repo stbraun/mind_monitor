17868080a07d1a30ae50bd7f7eb8f7376f8e0974
sb <sb@action.ms>
2014-10-18 20:39:24 +0200

    MongoDB support

---
+++ mind_monitor/monitoring.py
@@ -0,0 +1,15 @@
+"""Mind Monitoring.
+
+Great delta waves right from my brain :-)
+"""
+
+
+def great_stuff(port):
+    """Excellent stuff!
+
+    :param port: port number
+    :type port: str
+    :return: success
+    :rtype: boolean
+    """
+    return True
